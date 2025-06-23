58ab9fa8427b76064b97cff0f9e10ad1b8db72f5
Kent Adriane Goc-ong <kentadriane2@gmail.com>
2025-06-23 19:55:16 +0800

    style: Refine text-shadow properties in Reports and Dashboard CSS; adjust layout spacing in Reports component

--- client/src/Pages/Reports/Reports.jsx
+++ client/src/Pages/Reports/Reports.jsx
@@ -2,8 +2,8 @@ import './Reports.css';
 
 const Reports = () => {
   return (
-    <div className="d-flex flex-column gap-3 p-2">
-        <div className="d-flex justify-content-between">
+    <div className="d-flex flex-column gap-1 p-2">
+        <div className="d-flex justify-content-between mb-1">
             <h4>Reports & Analytics</h4>
             <div className="d-flex flex-wrap gap-2">
                 <button type="button" className="btn btn-primary f-size-14 fw-medium"><i className="bi bi-download me-1"></i> Export CSV</button>
@@ -13,7 +13,7 @@ const Reports = () => {
 
         { /*Reports card stats*/ }
         <div className='row'>
-            <div className='p-2 col-12 col-md-6 col-lg-3 '>
+            <div className='p-2 col-12 col-md-6 col-lg-3'>
                 <div className="d-flex align-items-center gap-3 border rounded-3 p-3 shadow-sm">
                     <div className='bg-primary-subtle rounded-3 reports-icon-container d-flex align-items-center justify-content-center'>
                         <i className="text-primary bi bi-bar-chart-line reports-card-icon"></i>
@@ -65,18 +65,69 @@ const Reports = () => {
 
         <div className='row'>
 
-            <div className="p-2 col-12 col-lg-6">
-                <div className='d-flex flex-column gap-3'>
+            <div className="col-12 col-lg-6 p-2">
+                <div className='d-flex flex-column border rounded-3 shadow-sm gap-3 p-4 h-100'>
                     <h4>Ticket Status Distribution</h4>
-                    <div className='d-flex flex-column gap-2'>
-                        <div className='d-flex justify-content-between fw-medium'>
-                            <div>Open</div>
-                            <div className='text-muted'>3 tickets</div>
+                    <div className='d-flex flex-column gap-3'>
+                        <div>
+                            <div className='d-flex justify-content-between fw-medium f-size-14'>
+                                <div className='mb-2'>Open</div>
+                                <div className='text-muted'>3 tickets</div>
+                            </div>
+                            <div className='ticket-status-progress bg-secondary-subtle rounded-3 overflow-hidden'>
+                                <div className='bg-primary h-100' style={{width: `${(3/10)*100}%`}}></div>
+                            </div>
                         </div>
-                        
+
+                        <div>
+                            <div className='d-flex justify-content-between fw-medium f-size-14'>
+                                <div className='mb-2'>In Progress</div>
+                                <div className='text-muted'>3 tickets</div>
+                            </div>
+                            <div className='ticket-status-progress bg-secondary-subtle rounded-3 overflow-hidden'>
+                                <div className='bg-warning h-100' style={{width: `${(3/10)*100}%`}}></div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className='d-flex justify-content-between fw-medium f-size-14'>
+                                <div className='mb-2'>Resolved</div>
+                                <div className='text-muted'>4 tickets</div>
+                            </div>
+                            <div className='ticket-status-progress bg-secondary-subtle rounded-3 overflow-hidden'>
+                                <div className='bg-success h-100' style={{width: `${(4/10)*100}%`}}></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <div className="col-12 col-lg-6 p-2">
+                <div className='d-flex flex-column border rounded-3 shadow-sm gap-3 p-4'>
+                    <h4>Team Overview</h4>
+
+                    <div className='d-flex flex-column gap-2'>
+                        <div className='d-flex align-items-center justify-content-between bg-body-secondary rounded-3 px-3 py-2'>
+                            <div className='fw-medium'><i className="bi bi-people text-primary team-overview-icon me-2"></i> Active Staffs</div>
+                            <div className='fw-bold f-size-16'>2</div>
+                        </div>
+                    </div>
+                    <div className='d-flex flex-column gap-2'>
+                        <div className='d-flex align-items-center justify-content-between bg-body-secondary rounded-3 px-3 py-2'>
+                            <div className='fw-medium'><i className="bi bi-people text-success team-overview-icon me-2"></i> Total Clients</div>
+                            <div className='fw-bold f-size-16'>2</div>
+                        </div>
+                    </div>
+                    <div className='d-flex flex-column gap-2'>
+                        <div className='d-flex align-items-center justify-content-between bg-body-secondary rounded-3 px-3 py-2'>
+                            <div className='fw-medium'><i className="bi bi-graph-up-arrow text-primary team-overview-icon me-2"></i> Ticket Today</div>
+                            <div className='fw-bold f-size-16'>2</div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            
 
         </div>
 
